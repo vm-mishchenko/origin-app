8e2a570e05cd81f9ddecb98bca11ecb9d091f2f0
vm-mishchenko <vm.mishchenko@gmail.com>
2019-02-15 16:46:27 -0800

    Add firebase file uploader

--- src/app/infrastructure/firebase-file-uploader/firebase-file-uploader.module.ts
+++ src/app/infrastructure/firebase-file-uploader/firebase-file-uploader.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FileUploaderService} from 'ngx-wall';
 import {FirebaseFileUploaderService} from './firebase-file-uploader.service';
 
 @NgModule({
     providers: [FirebaseFileUploaderService],
     imports: [
-        CommonModule
+        CommonModule,
+        FileUploaderService
     ]
 })
 export class FirebaseFileUploaderModule {
--- src/app/infrastructure/firebase-file-uploader/firebase-file-uploader.service.ts
+++ src/app/infrastructure/firebase-file-uploader/firebase-file-uploader.service.ts
@@ -1,15 +1,29 @@
 import {Injectable} from '@angular/core';
+import {AngularFireStorage} from '@angular/fire/storage';
 
 /*
-* Receive url, save - save it
+* Receive path and file - save it
 * No logic around unsuccessful operations
 * */
 @Injectable()
 export class FirebaseFileUploaderService {
-    constructor() {
+    constructor(private fireStorage: AngularFireStorage) {
     }
 
-    upload(): Promise<any> {
-        return Promise.resolve();
+    upload(path: string, file: File): Promise<string> {
+        const uploadTask = this.fireStorage.upload(path, file);
+
+        // only after file is uploaded we could get downloaded url
+        return uploadTask.then(() => {
+            // downloadURL could be obtained from firestorage reference
+            return this.fireStorage.ref(path).getDownloadURL()
+                .toPromise();
+        });
+    }
+
+    remove(path: string): Promise<any> {
+        return this.fireStorage.ref(path)
+            .delete()
+            .toPromise();
     }
 }
