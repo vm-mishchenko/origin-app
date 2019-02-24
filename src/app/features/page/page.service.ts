dbd866c150504439daeb1ca8dce518240847fd10
vm-mishchenko <vm.mishchenko@gmail.com>
2019-02-24 13:39:55 -0800

    Move page refactoring

--- src/app/features/page/page.service.ts
+++ src/app/features/page/page.service.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 import {WallModelFactory} from 'ngx-wall';
 import {Observable, Subject} from 'rxjs';
 import {Guid} from '../../infrastructure/utils';
-import {PAGE_BRICK_TAG_NAME} from '../page-ui/page-ui.constant';
 import {CreatePageAction} from './action/create-page.action';
+import {MovePageAction} from './action/move-page.action';
 import {RemovePageAction} from './action/remove-page.action';
 import {RemovePagesAction} from './action/remove-pages.action';
 import {DeletePageEvent} from './page-events.type';
@@ -37,102 +37,13 @@ export class PageService {
     }
 
     movePage(movedPageId: string, targetPageId: string = null): Promise<any> {
-        // cannot move page inside itself
-        if (movedPageId === targetPageId) {
-            return Promise.resolve();
-        }
-
-        return this.pageRepositoryService.getRelationPage(movedPageId).then((movedRelationPage) => {
-            // moved page already at the top
-            if (targetPageId === null) {
-                if (movedRelationPage.parentPageId === null) {
-                    return Promise.resolve();
-                }
-            }
-
-            return this.pageRepositoryService.getRelationPage(targetPageId).then((targetRelationPage) => {
-                // moved page already in target page
-                if (movedRelationPage.parentPageId === targetRelationPage.id) {
-                    return Promise.resolve();
-                }
-
-                return this.pageRepositoryService.getBodyPage(targetPageId).then((targetPageBodyPage) => {
-                    const targetParentPromises = [];
-                    // update new parent body
-                    // todo: duplicated code
-                    const targetWallModel = this.wallModelFactory.create({plan: targetPageBodyPage.body});
-                    targetWallModel.api.core.addBrickAtStart(PAGE_BRICK_TAG_NAME, {pageId: movedPageId});
-
-                    targetParentPromises.push(
-                        this.pageStorages.pageBodyStorage.update(targetPageBodyPage.id, {
-                            body: targetWallModel.api.core.getPlan()
-                        })
-                    );
-
-                    // update new parent relation
-                    targetParentPromises.push(
-                        this.pageStorages.pageRelationStorage.update(targetRelationPage.id, {
-                            childrenPageId: [
-                                ...targetRelationPage.childrenPageId,
-                                movedPageId
-                            ]
-                        })
-                    );
-
-                    // update moved page parent id
-                    targetParentPromises.push(
-                        this.pageStorages.pageRelationStorage.update(movedRelationPage.id, {
-                            parentPageId: targetPageId
-                        })
-                    );
-
-                    return Promise.all(targetParentPromises).then(() => {
-                        // if moved page was at the root level then
-                        // there is no need to update old parent page
-                        if (movedRelationPage.parentPageId === null) {
-                            return Promise.resolve();
-                        }
-
-                        // update old parent page
-                        return Promise.all([
-                            this.pageRepositoryService.getRelationPage(movedRelationPage.parentPageId),
-                            this.pageRepositoryService.getBodyPage(movedRelationPage.parentPageId)
-                        ]).then(([oldPageRelationPage, oldPageBodyPage]) => {
-                            const promises = [];
-
-                            // update old parent relation
-                            const movedPageChildIndex = oldPageRelationPage.childrenPageId.indexOf(movedPageId);
-                            promises.push(
-                                this.pageStorages.pageRelationStorage.update(oldPageRelationPage.id, {
-                                    childrenPageId: [
-                                        ...oldPageRelationPage.childrenPageId.slice(0, movedPageChildIndex),
-                                        ...oldPageRelationPage.childrenPageId.slice(movedPageChildIndex + 1),
-                                    ]
-                                })
-                            );
-
-                            // update old parent body
-                            // todo: duplicated code
-                            const oldWallModel = this.wallModelFactory.create({plan: oldPageBodyPage.body});
-                            oldWallModel.api.core
-                                .filterBricks((brick) => brick.tag === PAGE_BRICK_TAG_NAME && brick.state.pageId === movedPageId)
-                                .forEach((pageBrick) => {
-                                    oldWallModel.api.core.removeBrick(pageBrick.id);
-                                });
-
-                            promises.push(
-                                this.pageStorages.pageBodyStorage.update(oldPageBodyPage.id, {
-                                    body: oldWallModel.api.core.getPlan()
-                                })
-                            );
-
-                            return Promise.all(promises).then(() => {
-                            });
-                        });
-                    });
-                });
-            });
-        });
+        return (new MovePageAction(
+            movedPageId,
+            targetPageId,
+            this.pageStorages,
+            this.pageRepositoryService,
+            this.wallModelFactory
+        )).execute();
     }
 
     removePage(pageId: string): Promise<any> {
