a55b53047d2062fde45b0eb6297302d5ffd36648
Vitalii Mishchenko <vm.mishchenko@gmail.com>
2019-08-28 16:56:47 -0400

    Add ngx-wall-dev build

---
+++ scripts/ngx-wall-dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd ~/experients/ngx-wall
+# connect ngx-wall dist folder to origin project
+#  share ngx-wall dist folder
+mkdir -p dist/ngx-wall
+touch README.md
+cd dist/ngx-wall
+npm link
+#  link ngx-wall dist folder to origin-app
+cd ~/experients/origin-app
+npm link ngx-wall
+
+# launch ngx-wall dev process
+cd ~/experients/ngx-wall
+npm run build-for-origin
