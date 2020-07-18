474670d9db61398979ed7ea41df8d29fc69e4bb6
vm-mishchenko <vm.mishchenko@gmail.com>
2020-07-17 18:35:46 -0700

    Fix misspelling in ngx-wall-dev.sh script

--- scripts/ngx-wall-dev.sh
+++ scripts/ngx-wall-dev.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
-cd ~/experients/ngx-wall
 
-# connect $LIB dist folder to origin project
-# share $LIB dist folder
+# ngx-wall here represent a container that contains other libraries other then ngx-wall itself
+cd ~/experiments/ngx-wall
+
+echo "Connects $LIB library"
+
+## Connect $LIB "dist" folder to origin project
+# create $LIB "dist" folder if it has not existed yet
 mkdir -p dist/$LIB
 
-#  create fake file because if dist/$LIB is empty
-#  npm link parent $LIB folder instead of dist/$LIB
+# create fake file because if dist/$LIB is empty than
+# npm/link connects $LIB folder instead of dist/$LIB
 cp projects/$LIB/package.json dist/$LIB/package.json
 cd dist/$LIB
 npm link
 
 #  link $LIB dist folder to origin-app
-cd ~/experients/origin-app
+cd ~/experiments/origin-app
 npm link $LIB
 
 # launch ngx-wall dev process
 # I have to run it at the end because it has the "watch flag"
-cd ~/experients/ngx-wall
+cd ~/experiments/ngx-wall
 npm run build-for-origin
-
